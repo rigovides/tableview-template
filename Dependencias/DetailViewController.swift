56a303b01fa21350365b42d5695a705dba5242d2
Rigoberto Vides <rigoberto_videz@rover.com>
2019-08-03 11:00:57 -0700

    add after ignore

--- Dependencias/DetailViewController.swift
+++ Dependencias/DetailViewController.swift
@@ -12,29 +12,11 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
+    var detailItem: Any? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        configureView()
+        self.detailDescriptionLabel.text = detailItem as? String
     }
-
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
-        }
-    }
-
-
 }
 
