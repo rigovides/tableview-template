1d510ab1a8a6c51179ab00fa0125eca2c05c89dd
Rigoberto Vides <rigoberto_videz@rover.com>
2019-08-03 12:26:18 -0700

    Fetch dependencies from URL

--- Dependencias/MasterViewController.swift
+++ Dependencias/MasterViewController.swift
@@ -11,17 +11,18 @@ import UIKit
 class MasterViewController: UITableViewController {
     private let cellReuseIdentifier = "dependencyCell"
 
-    var objects: [[String: Any]] = [["Unidad de Registro Civil": "Morelos #65"]]
+    var dependencies: [[String: Any]] = [["Unidad de Registro Civil": "Morelos #65"]]
 
     override func viewDidLoad() {
          self.tableView.register(UINib(nibName: "DependencyTableViewCell", bundle: nil), forCellReuseIdentifier: self.cellReuseIdentifier)
+        self.fetchDendencies()
     }
 
     // MARK: - Segues
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as [String: Any]
+                let object = self.dependencies[indexPath.row] as [String: Any]
                 let controller = segue.destination as! DetailViewController
                 controller.detailItem = object
             }
@@ -30,7 +31,7 @@ class MasterViewController: UITableViewController {
 
     // MARK: - Table View
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return self.dependencies.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -38,5 +39,32 @@ class MasterViewController: UITableViewController {
         cell.mapIconImage.tintColor = UIColor(red: 0.7, green: 0.6, blue: 0.9, alpha: 1.0)
         //TODO: implement cell data population
         return cell
+    }
+}
+
+// MARK: - Networking code
+extension MasterViewController {
+    func fetchDendencies() {
+        let url = URL(string: "https://datos.guadalajara.gob.mx/sites/default/files/dependencias_municipales.geojson")
+
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            guard let data = data, error == nil else {
+                return
+            }
+
+            do {
+                let jsonResponse = try JSONSerialization.jsonObject(with: data, options: [])
+                let root = jsonResponse as? [String: Any]
+                self.dependencies = root?["features"] as! [[String: Any]]
+            } catch let parsingError {
+                print(parsingError)
+            }
+
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+
+        task.resume()
     }
 }
