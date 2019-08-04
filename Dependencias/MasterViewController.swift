4c1b24ae1d00d597d7bd74897915ee9cecef0936
Rigoberto Vides <rigoberto_videz@rover.com>
2019-08-04 12:16:28 -0700

    Add basic fetch. Mock getting dependencies.

--- Dependencias/MasterViewController.swift
+++ Dependencias/MasterViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = ["First item"]
+    var objects = [Dependency]()
     var persistanceContainer = PersistanceContainer.shared
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.fetchDependenciesIfNeeded()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        self.tableView.reloadData()
+    }
 
     // MARK: - Segues
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -33,9 +43,42 @@ class MasterViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let object = objects[indexPath.row]
-        cell.textLabel!.text = object
+        cell.textLabel!.text = object.name
+        cell.detailTextLabel?.text = object.address
         return cell
     }
+}
 
+extension MasterViewController {
+    private func fetchDependenciesIfNeeded() {
+        let request: NSFetchRequest<Dependency> = Dependency.fetchRequest()
+        guard let results = try? self.persistanceContainer.viewContext.fetch(request) as [Dependency], results.count > 0 else {
+            self.fetchDependencies()
+            return
+        }
+
+        self.objects = results
+    }
+
+    private func fetchDependencies() {
+        //get dependencies from any source
+        let mockResults = [("fake dependency A", "fake address A"), ("fake dependency B", "fake address B"), ("fake dependency C", "fake address C"), ("fake dependency D", "fake address D")]
+        //create managed objects and save them through context
+        for (name, address) in mockResults {
+            let dependency = Dependency(context: self.persistanceContainer.viewContext)
+            dependency.name = name
+            dependency.address = address
+        }
+
+        do {
+            try self.persistanceContainer.viewContext.save()
+        } catch {
+            fatalError("Failure to save context: \(error)")
+        }
+
+        if mockResults.count > 0 {
+            self.fetchDependenciesIfNeeded()
+        }
+    }
 }
 
