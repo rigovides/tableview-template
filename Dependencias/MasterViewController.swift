c763f4819b5a8d7b4f0adbe6d98ab9af2090dc85
Rigoberto Vides <rigovides@gmail.com>
2019-10-26 13:16:49 -0500

    Add indexes

--- Dependencias/MasterViewController.swift
+++ Dependencias/MasterViewController.swift
@@ -11,7 +11,42 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = ["First item"]
+    var objects = [["Alfredo", "Alicia", "Adrian"],
+                   ["Carlos", "Cesar", "Catalina"],
+                   ["Diego"],
+                   ["Francisco","Federico"],
+                   ["Lucia", "Luis", "Lola"],
+                   ["Rodrigo", "Rosa", "Ramon", "Ricardo"]]
+    
+    let sectionIndexes = ["A", "C", "D", "F", "L", "R"]
+    
+    var isEditingTable = false
+    
+    var objectsLabel: UILabel?
+    
+    var numberOfElements: Int {
+        return self.objects.compactMap{ $0.count }.reduce(0) { $0 + $1 }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let container = UIView(frame: CGRect(x: 0, y: 0, width: self.tableView.frame.width, height: 80.0))
+        container.backgroundColor = .black
+        
+        let objectsLabel = UILabel(frame: container.frame)
+        objectsLabel.textColor = .white
+        objectsLabel.font = UIFont.boldSystemFont(ofSize: 16.0)
+        objectsLabel.textAlignment = .center
+        
+        container.addSubview(objectsLabel)
+        
+        self.objectsLabel = objectsLabel
+        
+        self.tableView.tableFooterView = container
+        
+        self.objectsLabel?.text = "\(self.numberOfElements) Objects"
+    }
 
     // MARK: - Segues
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -25,16 +60,51 @@ class MasterViewController: UITableViewController {
     }
 
     // MARK: - Table View
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.objects.count
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return self.objects[section].count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let object = objects[indexPath.row]
+        let object = objects[indexPath.section][indexPath.row]
         cell.textLabel!.text = object
         return cell
     }
-
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionIndexes[section]
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return self.sectionIndexes
+    }
+  
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.objects[indexPath.section].remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .right)
+            self.objectsLabel?.text = "\(self.numberOfElements) Objects"
+        }
+    }
+    
+    @IBAction func editAction(_ sender: Any) {
+        self.isEditingTable = !self.isEditingTable
+        self.tableView.setEditing(self.isEditingTable, animated: true)
+        
+        guard let button = sender as? UIButton else {
+            return
+        }
+        
+        let title = self.isEditingTable ? "Done" : "Edit"
+        button.setTitle(title, for: .normal)
+    }
 }
 
